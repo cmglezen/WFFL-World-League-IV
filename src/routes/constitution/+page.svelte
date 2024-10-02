21c607c5ea584449c4e751c011300404fa013528
cmglezen <cmglezen@gmail.com>
2024-10-02 17:29:33 -0500

    Update +page.svelte

--- src/routes/constitution/+page.svelte
+++ src/routes/constitution/+page.svelte
@@ -1,12 +1,3 @@
-<script>
-    const today = new Date();
-    const resources = [
-        {
-            name: "League Bylaws",
-            url: "https://docs.google.com/document/d/1JjnK7s5GjissYzxJ6e71WqK47gvlhtgAxZcnpwO87zM/edit?usp=sharing",
-            icon: "insights",
-            premium: true,
-            separator: true,
-        },
-    ];
-</script>
+{
+                dest: `https://docs.google.com/document/d/1JjnK7s5GjissYzxJ6e71WqK47gvlhtgAxZcnpwO87zM/edit?usp=sharing`,
+            },
